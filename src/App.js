7c05ff06d4dcdbde9b80c4d47c7d62c072abf575
ChanSon0211 <111247124+ChanSon0211@users.noreply.github.com>
2022-11-12 19:46:39 +0700

    fix file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-import logo from './logo.svg';
+
 import React from "react";
-import './App.css';
-import MovieList from './componentsMovie/MovieList';
-// import header 
-// import Header from "./components/Header"
-import HomeEx2 from "./Ex2/Home"
-import ProductList from './components/ProductList';
-import StudentList from './components/StudentList';
+
 import ShoesList from './componentsShoes/ProductList';
 
-// import Footer from './components/Footer';
-// import Home from './components/Ex1/Home';
-// import DemoDataBlinding from './components/DemoDataBlinding';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <Header />
-        <Header />
-        <Header />
-        <Header />
-        <Header />
 
-        <Footer />
-        <Footer />
-        <Footer /> */}
-
-   {/* <DemoDataBlinding/>      */}
-{/* <Home/> */}
-{/* /* <HomeEx2/> */}
-
-{/* <ProductList/> */ }
-{/* <MovieList/> */}
-{/* 
-<StudentList/> */}
-
-<ShoesList/>
-
-
-        
-
+        <ShoesList />
 
       </div>
 
